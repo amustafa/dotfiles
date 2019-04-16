9395d690c1a9293d00c4aaf866d77ab2d3708dc6
Adam Mustafa <178707+amustafa@users.noreply.github.com>
2019-04-16 08:44:44 -0400

    update

--- ubuntu/external_as_home.sh
+++ ubuntu/external_as_home.sh
@@ -1,9 +1,9 @@
 #Taken from https://help.ubuntu.com/community/Partitioning/Home/Moving
 cp /etc/fstab /etc/fstab.$(date +%Y-%m-%d)
 
-echo "UUID=$HOME_DRIVE_UUID   /media/home    ext3          defaults       0       2" >> /etc/fstab
+echo "UUID=$HOME_DRIVE_UUID   /media/external_home    ext4          defaults       0       2" >> /etc/fstab
 
-sudo mkdir /media/home
+sudo mkdir /media/external_home
 
 sudo mount -a
 
