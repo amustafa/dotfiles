181cb3245f4001dcea1775fdf93a5da9d93ce9d7
Adam Mustafa <178707+amustafa@users.noreply.github.com>
2019-04-17 10:31:21 -0400

    update

--- neovim/install.sh
+++ neovim/install.sh
@@ -1,10 +1,15 @@
+sudo apt-get install neovim
 pip install neovim
 pip install flake8
 pip install jedi
+sudo apt-get install python3-neovim
 
 NVIM_HOME=${HOME}/.config/nvim
 git clone --depth=1 https://github.com/amix/vimrc.git $NVIM_HOME/vim_runtime
 sh $NVIM_HOME/vim_runtime/install_awesome_vimrc.sh
 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp my_plugins_config.vim $NVIM_HOME/vim_runtime
+cp my_configs.vim $NVIM_HOME/vim_runtime
+cp -r my_plugins.vim $NVIM_HOME/vim_runtime
 cp init.vim $NVIM_HOME
--- tmux/install.sh
+++ tmux/install.sh
@@ -1,6 +1,6 @@
 sudo apt-get install tmux
 
-
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # git clone https://github.com/gpakosz/.tmux.git
 cp tmux/tmux.conf ~/.tmux.conf
 cp tmux/tmux.conf.local ~/.tmux.conf.local
