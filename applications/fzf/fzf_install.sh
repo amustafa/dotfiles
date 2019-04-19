9501867765ddfc0740cb9cf4a000b0bae235197c
Adam Mustafa <178707+amustafa@users.noreply.github.com>
2019-04-19 10:57:22 -0400

    savepoint

--- applications/fzf/fzf_install.sh
+++ applications/fzf/fzf_install.sh
@@ -4,11 +4,11 @@ mkdir -p ${HOME}/opt
 FZF_HOME=${HOME}/opt/fzf
 
 git clone --depth 1 https://github.com/junegunn/fzf.git $FZF_HOME
-~/.fzf/install --no-bash --no-fish --64
+$FZF_HOME/install --no-bash --no-fish --no-zsh --64
 
-mv ${HOME}/.fzf $FZF_HOME
+#mv ${HOME}/.fzf $FZF_HOME
 
 # Add to user bin
 ln -s $FZF_HOME/bin/fzf ${HOME}/bin
 
-echo $FZF_HOME >> .zshrc.local
+#echo $FZF_HOME >> .zshrc.local
