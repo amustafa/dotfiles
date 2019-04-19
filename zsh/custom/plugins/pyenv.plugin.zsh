36b429fe2b9c5b863550ee87308e8de39f0de793
Adam Mustafa <178707+amustafa@users.noreply.github.com>
2019-04-19 13:57:35 -0400

    savepoint

--- zsh/custom/plugins/pyenv.plugin.zsh
+++ zsh/custom/plugins/pyenv.plugin.zsh
@@ -1,11 +1,4 @@
 # PyEnv plug
 
-if (`which pyenv`); then
-    echo 'pyenv installed'
-    eval "$(pyenv init -)"
-    eval "$(pyenv virtualenv-init -)"
-else 
-    echo "pyenv not installed"
-    echo `which pyenv`
-    echo "pyenv not installed"
-fi
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
