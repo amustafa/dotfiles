f8d6cb63078c69a1e612e5d1dcf109a50e18e0ef
Adam Mustafa <178707+amustafa@users.noreply.github.com>
2019-04-20 16:30:30 -0400

    savepoint

--- programming/node/node_install.sh
+++ programming/node/node_install.sh
@@ -1,4 +1,14 @@
-sudo apt-get install nodejs
-sudo apt-get install npm
-# https://github.com/creationix/nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+
+if [ -z ${NVM_DIR+x} ]; then
+    echo "NVM_DIR not set"
+else
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+    # git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+      # cd "$NVM_DIR"
+      # git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+    # \. "$NVM_DIR/nvm.sh"
+    # sudo apt-get install nodejs
+    # sudo apt-get install npm
+    # https://github.com/creationix/nvm
+fi
--- zsh/custom/plugins/my-node-conf.plugin.zsh
+++ zsh/custom/plugins/my-node-conf.plugin.zsh
@@ -1,17 +1,14 @@
 ########################################
 # Node
 ########################################
-if (`which node`); then 
-    NPM_PACKAGES="${HOME}/.npm-packages"
-    NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
+if [ -e $NVM_DIR ]; then 
+
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+    export NPM_PACKAGES="${HOME}/.npm-packages"
+    export NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
     export PATH="$NPM_PACKAGES/bin:$PATH"
 
     export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
-
-    # nvm 
-    if [ -z ${NVM_DIR+x} ]; then 
-        export NVM_DIR="$HOME/opt/nvm"
-    fi
-
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 fi
