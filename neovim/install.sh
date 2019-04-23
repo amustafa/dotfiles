59b7fb2fdd84c85222afa4d29353791d0573f13e
Adam Mustafa <178707+amustafa@users.noreply.github.com>
2019-04-23 16:23:32 -0400

    savepoint

--- neovim/install.sh
+++ neovim/install.sh
@@ -1,9 +1,12 @@
-python download_latest_neovim.py
+if [ "`uname -s`" = Darwin ]; then
+    brew install neovim
+else
+    python download_latest_neovim.py
+fi
 
 pip install neovim
 pip install flake8
 pip install jedi
-#sudo apt-get install python3-neovim
 
 NVIM_APP_DIR=${HOME}/opt
 NVIM_CONFIG_HOME=${HOME}/.config/nvim
@@ -13,17 +16,21 @@ if [ -e nvim.appimage ]; then
     chmod u+x nvim.appimage
     mv nvim.appimage $NVIM_APP_DIR/nvim/bin
     ln -s $NVIM_APP_DIR/nvim/bin/nvim.appimage $HOME/bin
-else
-    tar xzvf nvim-macos.tar.gz
-    rm nvim-macos.tar.gz
-    mv nvim-osx64 $NVIM_APP_DIR/nvim
-    ln -s $NVIM_APP_DIR/nvim/bin/nvim $HOME/bin
+# else USED WHEN DOWNLOADING
+#     tar xzvf nvim-macos.tar.gz
+#     rm nvim-macos.tar.gz
+#     mv nvim-osx64 $NVIM_APP_DIR/nvim
+#     ln -s $NVIM_APP_DIR/nvim/bin/nvim $HOME/bin
 fi
 
 git clone --depth=1 https://github.com/amix/vimrc.git $NVIM_CONFIG_HOME/vim_runtime
 sh $NVIM_CONFIG_HOME/vim_runtime/install_awesome_vimrc.sh
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Install vim.plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 cp my_plugins_config.vim $NVIM_CONFIG_HOME/vim_runtime
 cp my_configs.vim $NVIM_CONFIG_HOME/vim_runtime
 cp -r my_plugins.vim $NVIM_CONFIG_HOME/vim_runtime
