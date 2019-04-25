3a36c0a8c273c060a44c48f6fd1498c7910e9aee
Adam Mustafa <178707+amustafa@users.noreply.github.com>
2019-04-25 08:53:21 -0400

    savepoint

--- zsh/plugins/plugins_post_comp_init/pyenv.plugin.zsh
+++ zsh/plugins/plugins_post_comp_init/pyenv.plugin.zsh
@@ -1,6 +1,8 @@
 # PyEnv plug
 
-if [ -e $PYENV_ROOT ]; then 
+echo "Looking at pyenv"
+if [ -d $PYENV_ROOT ]; then 
+    echo "loading at pyenv"
     export PATH="${PYENV_ROOT}/bin:$PATH"
     eval "$(pyenv init -)"
     eval "$(pyenv virtualenv-init -)"
