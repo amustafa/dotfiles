1f5e2ec23a3dc57241f6ad63dcd712b61e3a463c
Adam Mustafa <178707+amustafa@users.noreply.github.com>
2019-11-06 09:24:14 -0500

    added mosh to ubuntu install

--- applications/basic_libs/basic_libs_ubuntu_install.sh
+++ applications/basic_libs/basic_libs_ubuntu_install.sh
@@ -1,4 +1,4 @@
 sudo apt-get install ubuntu-restricted-extras
-sudo apt-get install -y git terminator keepass2
+sudo apt-get install -y git terminator keepass2 mosh
 sudo apt-get install -y compizconfig-settings-manager
 sudo apt-get install -y apcalc silversearcher-ag libpcre3 libpcre3-dev ack autojump bat httpie mosh ncdu
